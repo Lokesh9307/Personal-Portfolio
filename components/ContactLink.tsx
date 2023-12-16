0d315c3306a68b2f559595599df9327c07f8d955
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2023-12-16 13:19:50 +0530

    Update ContactLink.tsx

--- components/ContactLink.tsx
+++ components/ContactLink.tsx
@@ -4,10 +4,10 @@ import { FaFacebookF ,FaInstagram,FaLinkedinIn ,FaGithub } from "react-icons/fa"
 
 const ContactLink = () => {
     const ContactLinks = [
-        {name:'Facebook',link:'https://instagram.com',icon:<FaFacebookF  className='text-blue-500 text-3xl bg-transparent'/>,color:'blue-slate-500'},
-        {name:'Instagram',link:'https://instagram.com',icon:<FaInstagram className='text-pink-500 text-3xl bg-transparent'/>,color:'pink-slate-500'},
-        {name:'LinkedIn',link:'https://instagram.com',icon:<FaLinkedinIn  className='text-blue-500 text-3xl bg-transparent'/>,color:'blue-slate-500'},
-        {name:'GitHub',link:'https://instagram.com',icon:<FaGithub className='text-white text-3xl bg-transparent'/>,color:'black'},
+        {name:'Facebook',link:'https://www.facebook.com/lokesh.umredkar.967/',icon:<FaFacebookF  className='text-blue-500 text-3xl bg-transparent'/>,color:'blue-slate-500'},
+        {name:'Instagram',link:'https://www.instagram.com/itz_lokesh_02/?hl=bg',icon:<FaInstagram className='text-pink-500 text-3xl bg-transparent'/>,color:'pink-slate-500'},
+        {name:'LinkedIn',link:'https://www.linkedin.com/in/lokesh-umredkar-2a0961243/?trk=people-guest_people_search-card&originalSubdomain=in',icon:<FaLinkedinIn  className='text-blue-500 text-3xl bg-transparent'/>,color:'blue-slate-500'},
+        {name:'GitHub',link:'https://github.com/Lokesh9307',icon:<FaGithub className='text-white text-3xl bg-transparent'/>,color:'black'},
       ]
     
   return (
