ec6196172ab3eedf2e369714315fee3c159c1475
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2024-03-09 22:32:43 +0530

    resume path

--- app/pdfview/page.tsx
+++ app/pdfview/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function PdfView() {
   return (
     <div className='h-[100vh] w-full flex justify-center items-center p-8 bg-[#212529]'>
-        <object data="./pdf/Lokesh_Umredkar.pdf" type="" className='w-50 h-full' width="900" height="900"></object>
+        <object data="/public/pdf/Resume.pdf" type="" className='w-50 h-full' width="900" height="900"></object>
     </div>
   )
 }
